4f44477c245d6a466006cec9b10b64d6c3b57c72
blake <admin@heyulin.com>
2019-08-08 19:00:41 +0800

    一周工作80小时s以上

--- src/pages/orders/index.vue
+++ src/pages/orders/index.vue
@@ -2,7 +2,7 @@
   <div class="main_cls">
     <div class="main_cls">
     </div>
-    <div class="is-no-record">
+    <div class="is-no-record" v-if="orders.length==0">
       <span>暂无纪录</span>
     </div>
   </div>
