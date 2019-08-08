b8cc8a80295edf2055fab0eba9cd34fb237c4e8e
blake <admin@heyulin.com>
2019-08-08 18:54:15 +0800

    一周工作80小时s以上

--- src/pages/orders/index.vue
+++ src/pages/orders/index.vue
@@ -57,8 +57,7 @@ export default {
     async onclickget() {
       t.toast('onclickget')
     },
-    async fetchProductOrder()
-    console.log(`###$fetchProductOrder;
+    async fetchProductOrder(){
       let res_products = await t.v2dispatch({
         type: `v2chuqidanopen`,
         payload: {
