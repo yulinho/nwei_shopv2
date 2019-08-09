5f6b8ed2f486b606da7b04181d1ac88c104eed34
blake <admin@heyulin.com>
2019-08-09 17:34:23 +0800

    一周工作80小时s以上

--- src/pages/orders/index.vue
+++ src/pages/orders/index.vue
@@ -93,6 +93,7 @@ export default {
         }
         if (foo.status == 'Zhuliing') {
           let order__id = data._id
+          await t.setItem('order__id',order__id)
           buttons.push({
             title: '查看助力情况',
             type: `primary`,
