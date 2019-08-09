72c809e398c1191d6971470ef71a6eacad7a738c
blake <admin@heyulin.com>
2019-08-09 23:00:12 +0800

    一周工作80小时s以上

--- src/App.vue
+++ src/App.vue
@@ -127,12 +127,12 @@ page {
 @font-face {
   font-family: 'iconfont';
   /* project id 1316788 */
-  src: url('//at.alicdn.com/t/font_1332581_plu7dgbkt2.eot');
-  src: url('//at.alicdn.com/t/font_1332581_plu7dgbkt2.eot?#iefix') format('embedded-opentype'),
-    url('//at.alicdn.com/t/font_1332581_plu7dgbkt2.woff2') format('woff2'),
-    url('//at.alicdn.com/t/font_1332581_plu7dgbkt2.woff') format('woff'),
-    url('//at.alicdn.com/t/font_1332581_plu7dgbkt2.ttf') format('truetype'),
-    url('//at.alicdn.com/t/font_1332581_plu7dgbkt2.svg#iconfont') format('svg');
+  src: url('//at.alicdn.com/t/font_1332581_nqvfa82hpik.eot');
+  src: url('//at.alicdn.com/t/font_1332581_nqvfa82hpik.eot?#iefix') format('embedded-opentype'),
+    url('//at.alicdn.com/t/font_1332581_nqvfa82hpik.woff2') format('woff2'),
+    url('//at.alicdn.com/t/font_1332581_nqvfa82hpik.woff') format('woff'),
+    url('//at.alicdn.com/t/font_1332581_nqvfa82hpik.ttf') format('truetype'),
+    url('//at.alicdn.com/t/font_1332581_nqvfa82hpik.svg#iconfont') format('svg');
 }
 
 .iconfont {
