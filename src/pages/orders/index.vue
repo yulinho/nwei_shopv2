8585f1faa9fa006d52b3fd99c89e50953b92db79
blake <admin@heyulin.com>
2019-08-09 15:48:56 +0800

    一周工作80小时s以上

--- src/pages/orders/index.vue
+++ src/pages/orders/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main_cls">
     <div class="main_cls" v-if="orders.length>0">
-      <xorder :args="{}"></xorder>
+      <xorder :args="{}" v-for="(item,index) in orders" :key="index"></xorder>
     </div>
     <div class="is-no-record" v-if="orders.length==0">
       <span>暂无纪录</span>
