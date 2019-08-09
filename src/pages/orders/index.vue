34578c5fb7253299a78292b0d7d80f6f20352b25
blake <admin@heyulin.com>
2019-08-09 17:17:20 +0800

    一周工作80小时s以上

--- src/pages/orders/index.vue
+++ src/pages/orders/index.vue
@@ -86,7 +86,7 @@ export default {
                 title: '支付',
                 type: `primary`,
                 click: ()=>{
-                  t.toast('支付')
+                  t.toast(`${支付}${foo.title}`)
                 }
               }
             ]
